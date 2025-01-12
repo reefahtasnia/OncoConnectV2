6d5340c86ad22e45e8dd908c27f4c21b8f926af0
mika-progga <pialeemoni@gmail.com>
2025-01-12 10:06:17 +0600

    progga
    mentalfixed

--- src/components/MentalHealthPage.js
+++ src/components/MentalHealthPage.js
@@ -6,6 +6,7 @@ import Expert3 from "./assets/team-3.jpg";
 import Expert4 from "./assets/team-4.jpg";
 import Footer from "./Footer";
 
+
 const MentalHealthPage = () => {
   return (
     <div className="mh-mental-health-container">
@@ -15,7 +16,12 @@ const MentalHealthPage = () => {
       {/* Hero Section */}
       <section className="mh-hero">
         <div className="mh-hero-content">
-          <h1>At the heart of <span className="mh-highlight">children & young people's</span> mental health</h1>
+        <h1>
+  At the heart of <br />
+  <span className="mh-highlight">children & young people's</span> <br />
+  mental health
+</h1>
+
           <button className="mh-find-therapist-button">Find a Therapist</button>
         </div>
         <div className="mh-hero-search">
